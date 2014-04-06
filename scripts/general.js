75a27074c71f708de2091e1d7961d4a47c53fc26
Emil Virkki <emil.virkki@gmail.com>
2014-04-07 00:22:39 +0300

    Tooltipit ikoneille

--- scripts/general.js
+++ scripts/general.js
@@ -1,11 +1,41 @@
 jQuery(function($) {
+  $('body').append('<script src="../scripts/tooltipster/js/jquery.tooltipster.min.js"></script>');
+  $('head').append('<link rel="stylesheet" href="../scripts/tooltipster/css/tooltipster.css">');
+
 	$('.collapsible .heading').click(function() {
 		$(this).parent().find('.content').slideToggle(function() {
 			$(this).parent().toggleClass('collapsed');
 		});
 
 	});
+
+  function setTitles() {
+  $('.icon-metsa').attr('title', 'Retkellä');
+  $('.icon-retki').attr('title', 'Retkellä');
+  $('.icon-kolo').attr('title', 'Kololla');
+  $('.icon-leiri').attr('title', 'Leirillä');
+  $('.icon-vene').attr('title', 'Veneessä');
+  $('.icon-ulkona').attr('title', 'Ulkona');
+  $('.icon-kaupunki').attr('title', 'PKaupungissa');
+  $('.icon-1h').attr('title', 'Yksin');
+  $('.icon-1tai2h').attr('title', 'Yksin tai kaksin');
+  $('.icon-ryhma').attr('title', 'Vartiossa');
+  $('.icon-isoryhma').attr('title', 'Joukkuessa tai isommassa ryhmässä');
+  $('.icon-pakollinen').attr('title', 'Pakollinen');
+  $('.icon-vapaavalintainen').attr('title', 'Vapaavalintainen');
+  $('.icon-valmisteluaika').attr('title', 'Valmisteluaika (minuuttia)');
+  $('.icon-kesto').attr('title', 'Kesto (minuuttia)');
+  $('.icon-taso').attr('title', 'Tarpojien taso');
+  }
+  setTitles();
+  $('.icon').tooltipster({
+    trigger: 'click'
+  });
+  setTitles();
+  
 });
+
+
 
 /*** GOOGLE ANALYTICS ***/
 
